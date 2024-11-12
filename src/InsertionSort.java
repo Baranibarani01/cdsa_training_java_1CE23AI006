2ffbdc6e98b1911fe3891e34f3c7673d3ccc7a6a
Baranibarani01 <bharani2244@gmail.com>
2024-11-12 15:21:53 +0530

    intel

---
+++ src/InsertionSort.java
@@ -0,0 +1,32 @@
+public class InsertionSort {
+    public static void InsertionSort(int[] arr){
+        for (int i=1;i<arr.length;i++){
+            int key = arr[i];
+            int j=i-1;
+
+            while (j>=0 && arr[j] > key){
+                arr[j +1]=arr[j];
+                j = j-1;
+            }
+            arr[j+1]=key;
+        }
+    }
+
+    public static void printArray(int[]arr){
+        for(int i=0;i< arr.length;i++){
+            System.out.print(arr[i] + "");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+
+        System.out.println("original array:");
+        printArray(arr);
+
+        InsertionSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
